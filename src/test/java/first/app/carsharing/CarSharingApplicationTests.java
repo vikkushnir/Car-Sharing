938a1a689480636eafd4771eb93b6dd8deeb1bb2
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-09-16 13:08:10 +0300

    Created Empty project

---
+++ src/test/java/first/app/carsharing/CarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package first.app.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
